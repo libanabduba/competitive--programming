913d8c4e090df22a715e1b888856302fb894a119
Liban <91590871+libanabduba@users.noreply.github.com>
2022-01-11 20:55:05 +0300

    Create predictWinner.py

---
+++ predictWinner.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def calc(nums,start,end):
+            if start == end:
+                return nums[start]
+            else:
+                return max(nums[start] -  calc(nums,start+1,end), nums[end] - calc(nums,start,end-1))
+            
+      
+        if calc(nums,0,len(nums) -1) >= 0:
+            return True
+        else:
+            return False
