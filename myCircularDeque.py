6351fff0ef691fa26362c6c01fe90ecde856a372
Liban <91590871+libanabduba@users.noreply.github.com>
2022-02-17 19:05:15 +0300

    Update myCircularDeque.py

--- myCircularDeque.py
+++ myCircularDeque.py
@@ -1,44 +1,51 @@
+ from collections import deque
  class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.node = []
-        self.max = k
+        self.queue = deque([])
+        self.size = k
+
     def insertFront(self, value: int) -> bool:
-        if len(self.node) < self.max:
-            self.node.insert(0,value)
+        if len(self.queue) < self.size:
+            self.queue.appendleft(value)
             return True
         return False
 
     def insertLast(self, value: int) -> bool:
-        if len(self.node) < self.max:
-            self.node.append(value)
+        if len(self.queue) < self.size:
+            self.queue.append(value)
             return True
         return False
+            
 
     def deleteFront(self) -> bool:
-        if len(self.node)>0:
-            self.node.pop(0)
+         if len(self.queue) != 0:
+            self.queue.popleft()
             return True
-        return False
+         return False
 
     def deleteLast(self) -> bool:
-        if len(self.node)>0:
-            self.node.pop()
+         if len(self.queue) != 0:
+            self.queue.pop()
             return True
-        return False
+         return False
 
     def getFront(self) -> int:
-        if len(self.node)>0:
-            return self.node[0]
+        if len(self.queue) != 0:
+            return self.queue[0]
         return -1
 
     def getRear(self) -> int:
-        if len(self.node)>0:
-            return self.node[-1]
+        if len(self.queue) != 0:
+            return self.queue[-1]
         return -1
 
     def isEmpty(self) -> bool:
-        return len(self.node)<=0
+         if len(self.queue) != 0:
+            return False
+         return True
 
     def isFull(self) -> bool:
-        return len(self.node) == self.max
+         if len(self.queue) == self.size:
+            return True
+         return False
