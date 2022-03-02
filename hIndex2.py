2bdcfcb0e3e7a4b1f05841c65dea0d9d33c20868
Liban <91590871+libanabduba@users.noreply.github.com>
2022-03-02 23:07:11 +0300

    Create  hIndex2.py

---
+++ hIndex2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        left = 0
+        right = len(citations) - 1
+        
+        while left <= right:
+            
+            mid = left + (right-left)//2
+            if len(citations) - citations[mid] == mid:
+                return citations[mid]
+            elif len(citations) - citations[mid] > mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+                    
+        return len(citations) - left
+                    
