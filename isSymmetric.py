daa788305203712b8fa9902c90f2687f1d45066c
Liban <91590871+libanabduba@users.noreply.github.com>
2022-03-06 23:52:21 +0300

    Create  isSymmetric.py

---
+++ isSymmetric.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(t1,t2):
+            if t1 == None and t2 == None: return True
+            
+            if t1 == None or t2 == None: return False
+            
+            return(t1.val == t2.val and isMirror(t1.left,t2.right)) and isMirror(t1.right,t2.left)
