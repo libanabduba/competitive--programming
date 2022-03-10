66199fc2e497b2e9afce46f92afdf14159b0380a
Liban <91590871+libanabduba@users.noreply.github.com>
2022-03-10 11:40:41 +0300

    Create removeDuplicateLetters.py

---
+++ removeDuplicateLetters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+     
+        count = Counter(s)
+        stack = []
+        
+        for i in s:
+            if i in stack:
+                count[i] -= 1
+                continue
+          
+            while stack and stack[-1] > i and count[stack[-1]] > 0:
+                stack.pop()
+            
+            stack.append(i)
+            count[i] -= 1
+        
+        return ''.join(stack)
+            
