9958725126db9f8b464256d70c20eee8ed7b4907
Liban <91590871+libanabduba@users.noreply.github.com>
2023-02-11 10:46:41 +0300

    Create 3-longest-substring-without-repeating-characters.py

---
+++ 3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+from collections import deque
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        check = deque()
+        for i in s:
+            if i in check:
+                ans = max(ans,len(check))
+                while i in check:
+                    check.popleft()
+            check.append(i)
+        return max(ans,len(check))
