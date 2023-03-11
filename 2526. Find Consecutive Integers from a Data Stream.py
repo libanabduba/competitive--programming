0689fec50d13bf791f3501795337532404cb53f3
Liban <91590871+libanabduba@users.noreply.github.com>
2023-03-11 23:30:01 +0300

    Create 2526. Find Consecutive Integers from a Data Stream.py

---
+++ 2526. Find Consecutive Integers from a Data Stream.py
@@ -0,0 +1,23 @@
+from collections import deque
+class DataStream:
+
+    def __init__(self, value: int, k: int):
+        self.queue = deque()
+        self.value = value
+        self.k = k
+        self.count = 0
+        
+
+    def consec(self, num: int) -> bool:
+        self.queue.append(num)
+        if num == self.value:
+            self.count += 1
+        else:
+            self.count = 0
+
+        return self.count >= self.k
+
+
+# Your DataStream object will be instantiated and called as such:
+# obj = DataStream(value, k)
+# param_1 = obj.consec(num)
