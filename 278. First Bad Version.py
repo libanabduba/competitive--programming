40f26c2e576aa4cd9cedfa1ae89a87a5049f8b00
Liban <91590871+libanabduba@users.noreply.github.com>
2023-03-18 15:59:02 +0300

    Create 278. First Bad Version.py

---
+++ 278. First Bad Version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+      
+        left=1
+        right=n
+        while left<=right:
+            mid = (left+right)//2
+            if(left==right):
+                return left
+            elif(isBadVersion(mid)==True):
+                right=mid
+            elif(isBadVersion(mid)==False):
+                left=mid+1
+           
+                
