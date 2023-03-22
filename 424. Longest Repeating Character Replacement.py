24cde66501a3c60b828a02f126e37437e2fa3be9
Liban <91590871+libanabduba@users.noreply.github.com>
2023-03-22 23:10:14 +0300

    Create 424. Longest Repeating Character Replacement.py

---
+++ 424. Longest Repeating Character Replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        dic = {}
+        size = 0
+        l = 0
+        
+        for i in range(len(s)):
+            
+            dic[s[i]] = 1 + dic.get(s[i],0)
+            
+            if (i - l + 1) - max(dic.values()) > k:
+                dic[s[l]] -= 1
+                l += 1
+                
+            size = max(size,i - l + 1)
+        return size
