93fb50481da2b70fe3f65ac57428586e80a89211
Liban Abduba <91590871+libanabduba@users.noreply.github.com>
2023-07-20 13:07:01 +0300

    Update 1971. Find if Path Exists in Graph.py

--- 1971. Find if Path Exists in Graph.py
+++ 1971. Find if Path Exists in Graph.py
@@ -32,6 +32,32 @@ class Solution:
 
         return bfs(source, destination)
 
+
+        # rank=[1]*n
+        # parent={i:i for i in range(n)}
+        # def find(ver):
+        #     res=ver
+        #     while res!=parent[res]:
+        #         parent[res]=parent[parent[res]]
+        #         res=parent[res]
+        #     return res
+        # def union(e1,e2):
+        #     pare1=find(e1) 
+        #     pare2=find(e2)  
+        #     if pare1!=pare2:
+        #         if rank[pare1] > rank[pare2]:
+        #             parent[pare2] = pare1
+        #             rank[pare1] += rank[pare2]
+        #         else:
+        #             parent[pare1] = pare2
+        #             rank[pare2] += rank[pare1]
+               
+        
+        # for u,v in edges:
+        #     union(u,v)       
+        
+        # return find(source)==find(destination)
+
           
 
             
